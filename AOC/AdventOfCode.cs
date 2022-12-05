d5110cd9157abd904971a5a9851dd40e4cc44b7a
Sammy Mahmoudi <>
2022-12-05 10:03:13 -0600

    Project Setup

---
+++ AOC/AdventOfCode.cs
@@ -0,0 +1,9 @@
+﻿using AOC.Challenges;
+
+namespace AOC {
+    class AdventOfCode {
+        static void Main(string[] args) {
+            P5.Run();
+        }
+    }
+}
---
+++ AOC/Challenges/P5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AOC.Challenges {
+    class P5 {
+        List<Stack<char>> AllStacks = new List<Stack<char>>{
+        new Stack<char>(new [] {'S', 'Z', 'P', 'D', 'L', 'B', 'F', 'C'}),
+        new Stack<char>(new [] {'N', 'V', 'G', 'P', 'H', 'W', 'B'}),
+        new Stack<char>(new [] {'F', 'W', 'B', 'J', 'G'}),
+        new Stack<char>(new [] {'G', 'J', 'N', 'F', 'L', 'W', 'C', 'S'}),
+        new Stack<char>(new [] {'W', 'J', 'L', 'T', 'P', 'M', 'S', 'H'}),
+        new Stack<char>(new [] {'B', 'C', 'W', 'G', 'F', 'S'}),
+        new Stack<char>(new [] {'H', 'T', 'P', 'M', 'Q', 'B', 'W'}),
+        new Stack<char>(new [] {'F', 'S', 'W', 'T'}),
+        new Stack<char>(new [] {'N', 'C', 'R'}),
+    };
+
+        public static void Run() {
+            Console.WriteLine("this is P5");
+        }
+    }
+}
